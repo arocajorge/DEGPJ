24ec86dabe4b0dd6f80f3634c807e41a7698b541
AROCA MAC <arocajorge92@gmail.com>
2019-06-21 11:49:10 -0500

    ,

---
+++ APPPJ/APPPJ/APPPJ/Models/UsuarioModel.cs
@@ -0,0 +1,16 @@
+﻿namespace APPPJ.Models
+{
+    using Newtonsoft.Json;
+    public class UsuarioModel
+    {
+        [JsonProperty("IdUsuario")]
+        public string IdUsuario { get; set; }
+        [JsonProperty("Nombre")]
+        public string Nombre { get; set; }
+        [JsonProperty("Clave")]
+        public string Clave { get; set; }
+        [JsonProperty("Estado")]
+        public bool Estado { get; set; }
+    }
+}
+
