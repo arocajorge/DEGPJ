319f7686792158f2ab8843fed88715504186a5b9
AROCA MAC <arocajorge92@gmail.com>
2019-07-04 11:25:10 -0500

    3

---
+++ APPPJ/APPPJ/APPPJ/ViewModels/CompraViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace APPPJ.ViewModels
+{
+    using APPPJ.Models;
+    using APPPJ.Views;
+    using APPPJ.Helpers;
+    using APPPJ.Services;
+    using System.Collections.ObjectModel;
+
+    public class CompraViewModel : BaseViewModel
+    {
+        #region Variables
+        private bool _IsEnabled;
+        private bool _IsVisible;
+        private DataAccess data;
+        private ApiService api;
+        #endregion
+
+        #region Propiedades
+        public bool IsEnabled
+        {
+            get { return this._IsEnabled; }
+            set { SetValue(ref this._IsEnabled, value); }
+        }
+        public bool IsVisible
+        {
+            get { return this._IsVisible; }
+            set { SetValue(ref this._IsVisible, value); }
+        }
+        #endregion
+
+        #region MyRegion
+        public CompraViewModel()
+        {
+            data = new DataAccess();
+            api = new ApiService();
+        }
+        #endregion
+
+    }
+}
---
+++ APPPJ/APPPJ/APPPJ/Views/CompraPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace APPPJ.Views
+{
+    public partial class CompraPage : ContentPage
+    {
+        public CompraPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
